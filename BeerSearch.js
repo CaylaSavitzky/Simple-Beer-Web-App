6b166c3735903131746402761e8d79efc6a96dbd
CaylaSavitzky <CaySavitzky@gmail.com>
2019-03-25 22:14:28 -0400

    V.n
    Favorites tabs working with searches and changing between topics and stuff.
    
    Next items:
    Fix long ass titles management
    favorites tab load more shouldn't work
    enter for searchbar
    search should check all_tab_checkbox

--- BeerSearch.js
+++ BeerSearch.js
@@ -1,9 +1,11 @@
-var results = [];
-var char_in_description_max = 134;
-var page = 1;
 var food = "chicken"
-var favorites = new Set();
-
+var favorites = new Map();
+favorites.set(food, new Set());
+var results = new Map();
+results.set(food,[]);
+var past_searches=[food];
+var page = 1;
+var char_in_description_max = 134;
 
 
 var show = () =>{
@@ -13,7 +15,7 @@ var show = () =>{
 var show_all = () =>{
 	clear_page();
 	let n = 0;
-  	results.forEach(function(item){
+  	results.get(food).forEach(function(item){
   		let card = make_card(item, n);
 	  	$( "#content" ).append(card);
 	  	n = n+1;
@@ -25,13 +27,14 @@ var show_all = () =>{
 
 var show_favorites = () =>{
 	clear_page();
-	let n = 0;
-  	favorites.forEach(function(number){
-  		let item = results[number]
-  		let card = make_card(item, number);
-	  	$( "#content" ).append(card);
-	  	n = n+1;
-	  });
+	favorites.forEach(function(value, key, map){
+		value.forEach(function(number){
+	  		let item = results.get(key)[number]
+	  		let card = make_card(item, number, key);
+		  	$( "#content" ).append(card);
+		});
+	})
+  	
   	setTimeout(show,100);
 }
 
@@ -41,16 +44,15 @@ var clear_page = () => {
 
 var refresh = () =>{
 	page = 1;
-	while (results.length < 0){results.pop();}
 	$( "#content" ).html("")
 }
 
-var make_card = (item, n) =>{
+var make_card = (item, n, card_food = food) =>{
 	let opening = "<div class = 'card'>";
 	let image = "<div class = 'card_image_holder'><img class = 'card_image' src = "+ item.image_url + "></div>";
 	let favorited = "";
-	if (favorites.has(n)){favorited = "checked"}
-	let fav_icon = "<input type='checkbox' class = 'favorite_checkbox'  " + favorited + " id = 'checkbox"+n+"'/><label class = 'favorite_icon' for = 'checkbox"+n+"' onclick = 'favorite(" + n +")'></label>"
+	if (favorites.get(card_food).has(n)){favorited = "checked"}
+	let fav_icon = "<input type='checkbox' class = 'favorite_checkbox'  " + favorited + " id = 'checkbox"+n+"'/><label class = 'favorite_icon' for = 'checkbox"+n+"' onclick = \"favorite(" + n +",  " + "'"+ card_food + "')\"></label>"
 	let title = item.name;
 	if (title.indexOf('-') != -1 ){ 
 		title = title.slice(0,title.indexOf('-'));}
@@ -72,10 +74,14 @@ var make_card = (item, n) =>{
 	return opening + image + card_text+closing;
 } 
 
-var favorite = (n) =>{
+var favorite = (n, food) =>{
 	box = $(".favorite_checkbox")[n];
-	if (box.checked == false){favorites.add(n);}
-	if (box.checked == true){favorites.delete(n);}
+	if (box.checked == false){favorites.get(food).add(n);}
+	if (box.checked == true){favorites.get(food).delete(n);}
+}
+
+var go_to_top = () =>{
+	$("body")[0].scrollIntoView({behavior: "smooth", block: "start"});
 }
 
 
@@ -88,9 +94,9 @@ var load_new = () =>{
 	  },
 	  success: function( result ) {
 	  	page = page+1;
-	  	let n = results.length;
+	  	let n = results.get(food).length;
 	  	result.forEach(function(item){
-	  		results.push(item);})
+	  		results.get(food).push(item);})
 	  	result.forEach(function(item){
 	  		let card = make_card(item, n);
 		  	$( "#content" ).append(card);
@@ -108,7 +114,7 @@ var search = () =>{
 	let tempfood = $("#search_bar_input")[0].value
 	if( tempfood != false){
 		food = tempfood;
-		tempfood = ''
+		tempfood = '';
 		for (i = 0; i < food.length; i++){
 			let letter = food[i];
 			if(letter != " "){
@@ -120,6 +126,16 @@ var search = () =>{
 		}
 		food = tempfood;
 	}
-	refresh();
-	load_new();
+	if (results.get(food) == undefined){
+		results.set(food,[]);
+		favorites.set(food, new Set());
+		refresh();
+		load_new();
+	}
+	else{
+		refresh();
+		show_all();
+	}
+	
+	
 }
